282f02695d93071b38893c4b09b8d413755d264a
Adrian <163891375+AdrianPalcic@users.noreply.github.com>
2024-06-01 13:59:36 +0200

    Delete program¸1 directory

--- program¸1/drugi.js
+++
@@ -1,5 +0,0 @@
-const clicker = document.querySelector(".clicker");
-
-clicker.addEventListener("click", () => {
-    location.href="index.html";
-})
--- program¸1/script.js
+++
@@ -1,6 +0,0 @@
-const bookButton = document.querySelector(".book");
-
-
-    bookButton.addEventListener("click", () => {
-        location.href="drugi.html", "_blank";
-    })
